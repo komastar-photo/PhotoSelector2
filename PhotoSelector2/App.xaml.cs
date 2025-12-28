317efae638d1b2c653f417b3d62ffa733e2be04a
Eugene Kim <komastar.dev@gmail.com>
2025-12-28 13:22:42 +0900

    init

---
+++ PhotoSelector2/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace PhotoSelector2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ PhotoSelector2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PhotoSelector2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
